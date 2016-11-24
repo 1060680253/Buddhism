7f0015265e1abb3162dcc82da25b19c91084f282
Jack Cheng <1060680253@qq.com>
2016-11-24 18:01:52 +0800

    company

--- app/src/main/java/com/yuanming/buddhism/module/mine/fragment/ActivityFragment.java
+++ app/src/main/java/com/yuanming/buddhism/module/mine/fragment/ActivityFragment.java
@@ -1,11 +1,14 @@
 package com.yuanming.buddhism.module.mine.fragment;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.yuanming.buddhism.R;
 import com.yuanming.buddhism.base.BaseFragment;
@@ -14,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by chenghuan on 2016/11/24.
@@ -25,6 +30,7 @@ public class ActivityFragment extends BaseFragment {
     ViewPager mViewPager;
     @BindView(R.id.materialup_tabs)
     TabLayout pagerStrip;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_mine_activity;
@@ -35,9 +41,22 @@ public class ActivityFragment extends BaseFragment {
         List<String> titles = new ArrayList<>();
         titles.add("参加");
         titles.add("发布");
-        TabPagerAdapter mPagerAdapter = new TabPagerAdapter(getChildFragmentManager(),titles);
+        TabPagerAdapter mPagerAdapter = new TabPagerAdapter(getChildFragmentManager(), titles);
         mViewPager.setAdapter(mPagerAdapter);
         pagerStrip.setupWithViewPager(mViewPager);
+    }
+
+    @OnClick({R.id.iv_back, R.id.iv_add})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                if(getFActivity()!=null){
+                    getFActivity().finish();
+                }
+                break;
+            case R.id.iv_add:
+                break;
+        }
     }
 
     public class TabPagerAdapter extends FragmentPagerAdapter {
@@ -45,7 +64,7 @@ public class ActivityFragment extends BaseFragment {
         FragmentManager fm;
         private List<String> titles;
 
-        public TabPagerAdapter(FragmentManager fm,List<String> title) {
+        public TabPagerAdapter(FragmentManager fm, List<String> title) {
             super(fm);
             this.titles = title;
             this.fm = fm;
@@ -72,7 +91,7 @@ public class ActivityFragment extends BaseFragment {
         }
     }
 
-    private Fragment getFragment(int index){
-        return new CountLogsFragment();
+    private Fragment getFragment(int index) {
+        return new ActivityListFragment();
     }
 }
